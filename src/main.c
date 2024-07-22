8eb6ffe665376ae45625a56820b268fe90b2f42e
Joel Longanecker <joel.longanecker@gmail.com>
2024-07-21 21:01:33 -0500

    making some sweet progress

--- src/main.c
+++ src/main.c
@@ -5,14 +5,15 @@
 typedef unsigned char u8;
 typedef unsigned short u16;
 
+
 #define BUFFER_WIDTH 20
 #define BUFFER_HEIGHT 18
 #define BUFFER_SIZE (BUFFER_HEIGHT * BUFFER_WIDTH)
 
 #define SNEK_DIR_UP 0
-#define SNEK_DIR_RIGHT 1
-#define SNEK_DIR_DOWN 2
-#define SNEK_DIR_LEFT 3
+#define SNEK_DIR_DOWN 1
+#define SNEK_DIR_LEFT 2
+#define SNEK_DIR_RIGHT 3
 
 const u8 TILE_SNEK[] = {0x01, 0x02};
 const u8 TILE_SCORE[] = {0x03, 0x04, 0x05};
@@ -20,16 +21,15 @@ const u8 TILE_HEART[] = {0x06};
 const u8 TILE_DIAMOND[] = {0x07};
 
 const u8 TILE_SCORENUMS[] = {0x08, 0x09, 0x0A, 0x0B, 0x0C,
-                             0x0D, 0x0E, 0x0F, 0x10, 0x12};
+                             0x0D, 0x0E, 0x0F, 0x10, 0x11};
 
-const u8 TILE_SNAKE_HEAD[] = {0x13, 0x14, 0x15, 0x16};
-const u8 TILE_SNAKE_LR[] = {0x17};
-const u8 TILE_SNAKE_UD[] = {0x18};
-const u8 TILE_SNAKE_TAIL[] = {0x19, 0x1A, 0x1B, 0x1C};
+const u8 TILE_SNEK_HEAD[] = {0x12, 0x13, 0x14, 0x15};
+const u8 TILE_SNEK_LR[] = {0x16};
+const u8 TILE_SNEK_UD[] = {0x17};
+const u8 TILE_SNEK_TAIL[] = {0x18, 0x19, 0x1A, 0x1B};
 
 void seed_game();
 void seed_round();
-
 void process_game();
 void process_round();
 void process_round_input();
@@ -40,12 +40,12 @@ Global Variables
 */
 
 u8 snekTileBuffer[BUFFER_SIZE]; // The tiles making up the level
-u8 snekBodyBuffer[BUFFER_SIZE]; // The tiles making up the snake body
+u8 snekBodyBuffer[BUFFER_SIZE]; // The tiles making up the snek body
 u8 finalBuffer[BUFFER_SIZE];    // The tiles that will be coppied to memory
 
-u8 snekX; // position of the snake
+u8 snekX; // position of the snek
 u8 snekY;
-u8 snekDir;   //  the direction the snake is facing
+u8 snekDir;   //  the direction the snek is facing
 u8 snekScore; //  the score
 u8 snekLives; //  the lives left
 u8 snekLevel; //  what level we are on
@@ -90,10 +90,38 @@ void process_round() {
   // process the round input
 
   do {
-    process_round_input();
 
-    // move the snake a little bit
+    for (u8 inptCounter = 0; inptCounter <= 3; inptCounter++) {
+
+      process_round_input();
+
+      // update the buffer
+      u16 snekHead = snekY * BUFFER_WIDTH + snekX;
+      for (u16 i = 0; i < BUFFER_SIZE; i++) {
+        finalBuffer[i] = snekTileBuffer[i];
+        if (snekBodyBuffer[i] > 0) {
+          finalBuffer[i] = TILE_DIAMOND[0];
+        }
+        if (snekHead == i ) {
+
+          finalBuffer[i] = TILE_SNEK_HEAD[snekDir];
+        }
+      }
+
+      set_tile_map(0, 0, BUFFER_WIDTH, BUFFER_HEIGHT, finalBuffer);
+    }
+
     process_round_movement();
+
+    for (u16 i = 0; i < BUFFER_SIZE; i++) {
+
+      if (snekBodyBuffer[i] > 0) {
+        snekBodyBuffer[i] = snekBodyBuffer[i] - 1;
+      }
+    }
+
+    // delay(100);
+    vsync();
   } while (process_round_collision());
 
   // if we left the loop that means we dead
@@ -111,6 +139,10 @@ u8 process_round_collision() {
   if (snekBodyBuffer[playerLinearPos]) {
     return 0;
   }
+
+  snekBodyBuffer[playerLinearPos] = snekLevel * 3 + 15;
+  snekScore++;
+
   return 1;
 }
 
@@ -121,7 +153,7 @@ void process_round_movement() {
     snekY--;
     break;
   case SNEK_DIR_DOWN:
-    snekY--;
+    snekY++;
     break;
   case SNEK_DIR_LEFT:
     snekX--;
@@ -148,9 +180,9 @@ void process_round_input() {
 
 void main() {
 
-  set_tile_data(0, 41, (u8 *)snektiles, 0);
-
   DISPLAY_ON;
+  SHOW_BKG;
+  set_bkg_data(0, 41, (u8 *)snektiles);
 
   while (1) {
     seed_game();
